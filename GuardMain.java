702a53464ced76d94a02cbc17ade1ab89eccb6f4
SCJ-dev <sc262352@gmail.com>
2026-04-13 22:06:41 +0900

    리눅스 환경에서 작성한 경비 시스템입니다. 하드웨어에서 신호가 떨어졌다는 가정하에 움직이는 시스템입니다.

---
+++ GuardMain.java
@@ -0,0 +1,59 @@
+package java_project;
+
+class Sensor {
+	private String name;
+	private boolean isActive;
+	private int meter;
+	public Sensor (String name, boolean isActive,int meter){
+		this.name = name;
+		this.isActive = true;
+		this.meter=meter;
+	}
+	public void operate() {
+		if (isActive == true){
+			System.out.println("이 센서에 이름은 "+name+"입니다.\n현재 가동중입니다.");
+		} else {
+			System.out.println("시스템이 가동되지 않습니다.");
+		}
+	}
+}
+
+class PIRSensor extends Sensor{
+	private int detectionRange;
+	public PIRSensor(String name, boolean isActive, int detectionRange){
+		super(name, isActive, detectionRange);
+		this.detectionRange=detectionRange;
+	}
+	@Override
+	public void operate() {		
+		System.out.println("[알림] 적외선으로 움직임을 포착했습니다 경보를 울립니다.\n 전방 : "+detectionRange+"m 방면");
+		}
+}
+class SoundSensor extends Sensor {
+	private int threshold;
+	public SoundSensor (String name, boolean isActive, int threshold){
+		super(name, isActive, threshold);
+		this.threshold=threshold;
+	}
+	@Override
+	public void operate() {
+		System.out.println("[알림] 일정 데시벨 이상의 소음을 감지했습니다. 주변을 확인하세요.\n 현재 데시벨 : "+threshold);
+	}
+}
+
+public class GuardMain {
+	public static void main(String[] args) {
+		System.out.println("===== 무인 경비 시스템 가동 =====");
+		Sensor security = new Sensor("cam", true, 50);
+		security.operate();
+		SoundSensor sound = new SoundSensor("cam", true, 100);
+		System.out.println();
+	
+		sound.operate();
+		System.out.println("==================");
+		PIRSensor pir = new PIRSensor("cam", true, 500);
+		pir.operate();
+		System.out.println();
+		System.out.println("=====시스템 점검 완료=====");
+	}
+}
